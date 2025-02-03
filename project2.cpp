e9d726519ec8596e4d37c5fb20661864b5f36418
sheep <pierre.coric.lg@gmail.com>
2025-02-03 15:48:57 -0500

    project 2 created

---
+++ project2.cpp
@@ -0,0 +1,28 @@
+/*
+2. Write a function called merge_lists that takes two call-by-reference argu-
+ments that are pointer variables that point to the heads of linked lists of
+values of type int. The two linked lists are assumed to be sorted so that the
+number at the head is the smallest number, the number in the next node is
+the next smallest, and so forth. The function returns a pointer to the head
+of a new linked list that contains all of the nodes in the original two lists.
+The nodes in this longer list are also sorted from smallest to largest values.
+Note that your function will neither create nor destroy any nodes. When
+the function call ends, the two pointer variable arguments should have the
+value NULL.
+*/
+
+template <class T>
+class Node {
+    private:
+    public:
+};
+
+template <class T>
+class List {
+    private:
+    public:
+};
+
+int main() {
+    return 0;
+}
