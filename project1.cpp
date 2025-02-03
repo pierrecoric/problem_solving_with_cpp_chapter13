cad161520b2aa14e66c6f7f691dccebe8081eee8
sheep <pierre.coric.lg@gmail.com>
2025-02-03 10:34:09 -0500

    rewriting project1

--- project1.cpp
+++ project1.cpp
@@ -9,244 +9,98 @@ simply rearrange nodes. Place your function in a suitable test program.
 */
 
 #include <iostream>
-#include <string>
-using namespace std;
+using std::cout;
+using std::cin;
+using std::ostream;
+using std::istream;
 
+template <class T>
 class Node;
+template <class T>
 class List;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////Node 
+template <class T>
 class Node {
     private:
-        string data;
+        T data;
         Node* link;
     public:
-        //simple constructor.
-        Node() : data(""), link(nullptr) {};
+        //Default constructor.
+        Node();
+        //Constructor with a data parameter.
+        Node(T t);
         //Copy constructor.
-        Node(const Node& original); 
-        //Constructor with string parameter.
-        Node(string s);
-        ~Node() {};
-        Node& operator=(const Node& rhs);
-        string const getData() {return data;}
-        void setData(string s) {data = s;}
-        Node* const getLink() {return link;}
-        void setLink(Node* l) {link = l;}
-        friend List;
+        Node(const Node<T>& n);
+        //Destructor.
+        ~Node() {}
+
+        //Overloading <<cout>>.
+        template <class V>
+        friend ostream& operator <<(ostream& outs, const Node<V>& n);
+
+        //Members function.
+        void setData(T t) {data = t;}
+        T getData() const {return data;}
+
+        //Make list a friend of Node.
+        friend List<T>;
 };
 
-Node::Node(string s) {
-    data = s;
+//Default constructor.
+template <class T>
+Node<T>::Node() {
     link = nullptr;
+    data = NULL;
 }
 
-Node::Node(const Node& original) {
-    data = original.data;
-    link = original.link;
+//Constructor with a data parameter.
+template <class T>
+Node<T>::Node(T t) {
+    link = nullptr;
+    data = t;
 }
 
+template <class T>
+Node<T>::Node(const Node<T>& n) {
+    data = n.data;
+    link = n.link;
+}
 
-typedef Node* nodePtr;
+template <class T>
+ostream& operator <<(ostream& outs, const Node<T>& n) {
+    outs << n.data;
+    return outs;
+}
 
+////////////////////////////////////////////////////////////////////////////////////////////////////Node
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////List
+template <class T>
 class List {
     private:
-        nodePtr head;
+        Node<T>* head;
         int size;
     public:
-        List(): head(nullptr), size(0) {};
-        List(const List& original);
-        List& operator=(const List& rhs);
+        List();
+        List(const List<T>& original);
         ~List() {clear();}
-        void insertAtHead(string data);
-        void print();
+
+        template <class V>
+        friend ostream& operator <<(ostream& outs, const List<V>& l);
+
         void clear();
         int getSize() {return size;}
-        nodePtr const getHead() {return head;}
-        void setHead(nodePtr ptr) {head = ptr;}
-        void reverse();
-        List deepCopy();
+        void reverse(); 
 };
 
-//Copy constructor to create a deep copy of the list.
-List::List(const List& original) {
-    head = nullptr;
-    size = 0;
-
-    nodePtr current = original.head;
-    nodePtr last = nullptr;
-
-    while(current != nullptr) {
-        //Creating a new node and copying the data.
-        nodePtr newNode = new Node;
-        newNode -> data = current -> data;
-        if(head == nullptr) {
-            head = newNode;
-        }
-        else {
-            last -> link = newNode;
-        }
-        last = newNode;
-        current = current -> link;
-        size ++;
-    }
-
-}
-
-//Overloading the = operator
-List& List::operator=(const List& rhs) {
-    //Check for self assignment:
-    if(this == &rhs) {
-        return *this;
-    }
-
-    clear();
-    
-    nodePtr current = rhs.head;
-    nodePtr last = nullptr;
-
-    while(current != nullptr) {
-        //Creating a new node and copying the data.
-        nodePtr newNode = new Node;
-        newNode -> data = current -> data;
-        if(head == nullptr) {
-            head = newNode;
-        }
-        else {
-            last -> link = newNode;
-        }
-        last = newNode;
-        current = current -> link;
-        size ++;
-    }
-    return *this;
-}
-
-List List::deepCopy() {
-    List l;
-
-    nodePtr current = head;
-    nodePtr last = nullptr;
-
-    while(current != nullptr) {
-        nodePtr newNode = new Node;
-        newNode -> data = current -> data;
-        if(l.getHead() == nullptr) {
-            l.setHead(newNode);
-        }
-        else {
-            last -> link = newNode;
-        }
-        last = newNode;
-        current = current -> link;
-        l.size ++;
-    }
-    return l;
-}
-
-void List::insertAtHead(string data) {
-    nodePtr temp = new Node(data);
-    if(size == 0) {
-        head = temp;
-    }
-    else {
-        temp -> link = head;
-        head = temp;
-    }
-    size ++;
-}
-
-void List::print() {
-    if(size == 0) {
-        cout << "Empty list." << endl;
-        return;
-    }
-    nodePtr temp = head;
-    while (temp != nullptr) {
-        cout << temp -> data << endl;
-        temp = temp -> link;
-    } 
-}
-
-void List::clear() {
-    while(head != nullptr) {
-        nodePtr temp = head;
-        head = head -> link;
-        delete temp;
-    }
-}
-
-void List::reverse() {
-    nodePtr previous(nullptr), current(head), next(nullptr);
-    while(current != nullptr) {
-        //Store the next one.
-        next = current -> link;
-
-        //Reverse the node pointer of the current node to point to the previous node.
-        current -> link = previous;
-
-        //Move the pointers
-        previous = current;
-        current = next;
-    }
-    head = previous;
-}
-
-nodePtr reverse(nodePtr head) {
-    //Declaring and initializing the pointers.
-    nodePtr previous(nullptr), current(head), next(nullptr);
-    while (current != nullptr) {
-        //Store the next one
-        next = current->getLink();
-
-        //Reverse the node pointer of the current Node;
-        current->setLink(previous);
-
-        //Move the pointers 
-        previous = current;
-        current = next;
-    }
-    return previous;
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////List
 
 int main() {
-    Node n("test");
-    cout << n.getData();
-    Node m(n);
-    cout << m.getData();
-    /*
-    List l;
-    cout << "Testing program" << endl;
-    l.insertAtHead("first");
-    l.insertAtHead("second");
-    l.insertAtHead("third");
-    l.insertAtHead("fourth");
-    l.print();
-    cout << "Clearing the list" << endl;
-    l.clear();
-    l.clear();
-    l.print();
-    l.insertAtHead("first");
-    l.insertAtHead("second");
-    l.insertAtHead("third");
-    l.insertAtHead("fourth");
-    l.print();
-    cout << "Testing the reverse function:" << endl;
-    l.setHead(reverse(l.getHead()));
-    l.print();
-    cout << "Reversing again:" << endl;
-    l.reverse();
-    l.print();
-    cout << "Attempting copy:" << endl;
-    List l2(l);
-    l2.print();
-    cout << "New Insertion" << endl << endl;
-    l2.insertAtHead("COUCOU");
-    l = l2;
-    cout << "assigning l2 to l and printing it" << endl << endl;
-    l.print();
-    cout << "Ok more tests" << endl << endl;
-    List l3;
-    l3 = l.deepCopy();
-    l3.print();
-    */
+    Node<int> i(10);
+    Node<char> c('c');
+    cout << i << "\n";
+    cout << c << "\n";
     return 0;
 }
