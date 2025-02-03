a0d3efe06baaa13c64c8d9f9a999b30b1f69bb2f
sheep <pierre.coric.lg@gmail.com>
2025-02-03 13:06:43 -0500

    better

--- project1.cpp
+++ project1.cpp
@@ -39,6 +39,10 @@ class Node {
         template <class V>
         friend ostream& operator <<(ostream& outs, const Node<V>& n);
 
+        //Making the overloading of << a friend of node as well so it can access the data.
+        template <class V>
+        friend ostream& operator <<(ostream& outs, const List<V>& l);
+
         //Members function.
         void setData(T t) {data = t;}
         T getData() const {return data;}
@@ -83,17 +87,80 @@ class List {
         Node<T>* head;
         int size;
     public:
-        List();
+        List() : head(nullptr), size(0) {}
         List(const List<T>& original);
         ~List() {clear();}
 
         template <class V>
         friend ostream& operator <<(ostream& outs, const List<V>& l);
 
+        void insertAtHead(T t);
         void clear();
         int getSize() {return size;}
-        void reverse(); 
+        void reverse();
+        friend Node<T>; 
 };
+
+template <class T>
+List<T>::List(const List<T>& original) {
+    head = nullptr;
+    size = original.size;
+
+    Node<T>* current = original.head;
+    Node<T>* last = nullptr;
+    
+    while(current != nullptr) {
+        Node<T>* newNode = new Node<T>(current -> data);
+        if(head == nullptr) {
+            head = newNode;
+        }
+        else {
+            last -> link = newNode;
+        }
+        last = newNode;
+        current = current -> link;
+    }
+}
+
+//Overloading <<.
+template <class T>
+ostream& operator <<(ostream& outs, const List<T>& l) {
+    Node<T>* current = l.head;
+    int count = 0;
+    outs << "[";
+    while(current != nullptr) {
+        outs << current -> data;
+        if (count < l.size - 1) {
+            outs << ", ";
+        }
+        count ++;
+        current = current -> link;
+    }
+    outs << "]";
+    return outs;
+}
+
+template <class T>
+void List<T>::insertAtHead(T t) {
+    Node<T>* temp = new Node(t);
+    if(size == 0) {
+        head = temp;
+    }
+    else {
+        temp -> link = head;
+        head = temp;
+    }
+    size ++;
+}
+
+template <class T>
+void List<T>::clear() {
+    while(head != nullptr) {
+        Node<T>* temp = head;
+        head = head -> link;
+        delete temp;
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////List
 
@@ -102,5 +169,18 @@ int main() {
     Node<char> c('c');
     cout << i << "\n";
     cout << c << "\n";
+    List<int> li;
+    li.insertAtHead(10);
+    li.insertAtHead(25);
+    li.insertAtHead(55);
+    li.insertAtHead(23);
+    li.insertAtHead(55);
+    cout << li << "\n";
+    List<char> lc;
+    lc.insertAtHead('a');
+    lc.insertAtHead('c');
+    lc.insertAtHead('a');
+    lc.insertAtHead('c');
+    cout << lc << "\n";
     return 0;
 }
