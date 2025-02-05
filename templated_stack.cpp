4991d1ff6f303e2b8003e60f328b119bbb037b38
Pierre Coric <pierrecoric@Elizas-MBP.lan>
2025-02-05 21:18:14 +0100

    works fine

--- templated_stack.cpp
+++ templated_stack.cpp
@@ -37,8 +37,11 @@ class Node {
         //Overloading <<
         template <class N>
         friend ostream& operator <<(ostream& outs, const Node<N>& node);
+        //Declaring << for Stack and Queue here as well.
         template <class S>
         friend ostream& operator <<(ostream& outs, const Stack<S>& s);
+        template <class Q>
+        friend ostream& operator <<(ostream& outs, const Queue<Q>& s);
         
         friend class Stack<T>;
         friend class Queue<T>;
@@ -81,19 +84,23 @@ class Stack {
         Node<T>* top;
         int size;
     public:
+        //Default constructor.
         Stack() : top(nullptr), size(0) {}
+        //Copy constructor.
         Stack(const Stack<T>& original);
+        //Destructor.
         ~Stack(){ clear();}
 
         //Members functions
-        int getSize() {return size;}
-        bool empty() {return size == 0;}
+        int getSize() const {return size;}
+        bool empty() const {return size == 0;}
         //Push function.
         void push(T d);
         //Pop function.
         T pop();
         //Clear function.
         void clear();
+        void reverse();
 
         //Overloading the assignment operator
         Stack<T>& operator =(const Stack<T>& rhs);
@@ -105,6 +112,7 @@ class Stack {
         friend class Node<T>;
 };
 
+//Copy constructor
 template <class T>
 Stack<T>::Stack(const Stack<T>& original) {
     top = nullptr;
@@ -115,10 +123,14 @@ Stack<T>::Stack(const Stack<T>& original) {
         if(top == nullptr) {
             top = newNode;
         }
-        top = top -> link;
+        else {
+            newNode -> link = top;
+            top = newNode;
+        }
         current = current -> link;
         size ++;
     }
+    reverse();
 }
 
 //Push function.
@@ -156,39 +168,265 @@ void Stack<T>::clear() {
     top = nullptr;
 }
 
+//Reverse function
+template <class T>
+void Stack<T>::reverse() {
+    if(size < 2) {
+        return;
+    }
+    Node<T>* previous = nullptr;
+    Node<T>* current = top;
+    Node<T>* next = nullptr;
+    
+    while(current) {
+        //Store the next node.
+        next = current -> link;
+        //Invert the current node's pointer
+        current -> link = previous;
+
+        //Move the pointers
+        previous = current;
+        current = next;
+    }
+    top = previous;
+}
+
+//Overload the assignment operator.
+template <class T>
+Stack<T>& Stack<T>::operator =(const Stack<T>& rhs) {
+    if(this != &rhs) {
+        clear();
+        Node<T>* current = rhs.top;
+        while(current) {
+            Node<T>* newNode = new Node<T>(current -> data);
+            if(!top) {
+                top = newNode;
+            }
+            else {
+                newNode -> link = top;
+                top = newNode;
+            }
+            current = current -> link;
+            size ++;
+        }
+    }
+    reverse();
+    return *this;
+}
+
+
+//Overloading <<.
 template <class T>
 ostream& operator <<(ostream& outs, const Stack<T>& s) {
     Node<T>* current = s.top;
     int count(0);
     outs << "_\n";
     while(current) {
-        outs << current -> data << "\n";
+        outs << *current << "\n";
         current = current -> link;
         if(count < s.size - 1) {
             outs << "|\n";
             count ++;
         }
     }
-    outs << "_";
+    outs << "-";
     return outs;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////Stack
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////Queue
 template <class T>
 class Queue {
     private:
+        Node<T>* head;
+        Node<T>* tail;
+        int size;
     public:
+        //Default constructor.
+        Queue() : head(nullptr), tail(nullptr), size(0) {}
+        //Copy constructor.
+        Queue(const Queue<T>& original);
+        ~Queue() {clear();}
+
+        //Overloading the assignment operator
+        Queue<T>& operator =(const Queue<T>& rhs);
+
+        //Members functions.
+        int getSize() const {return size;}
+        bool empty() const {return size == 0;}
+        //enqueue function.
+        void enqueue(T d);
+        //dequeue function.
+        T dequeue();
+        //Clear function.
+        void clear();
+        void reverse();
+
+        //Overloading the << operator.
+        template <class Q>
+        friend ostream& operator <<(ostream& outs, const Queue<Q>& q);
+
+        friend class Node<T>;
+
 };
+
+//Copy constructor.
+template <class T>
+Queue<T>::Queue(const Queue<T>& original) {
+    head = nullptr;
+    tail = nullptr;
+    size = 0;
+    if(original.head) {
+        Node<T>* current = original.head;
+        while(current) {
+            Node<T>* newNode = new Node<T>(current -> data);
+            if(head == nullptr) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                newNode -> link = head;
+                head = newNode;
+            }
+            current = current -> link;
+            size ++;
+        }
+    }
+}
+
+//Overloading the assignment operator
+template <class T>
+Queue<T>& Queue<T>::operator =(const Queue<T>& rhs) {
+    if(this != &rhs) {
+        clear();
+        Node<T>* current = rhs.head;
+        while(current) {
+            enqueue(current -> data);
+            current = current -> link;
+        }
+    }
+    return *this;
+}
+
+//enqueue function.
+template <class T>
+void Queue<T>::enqueue(T d) {
+    Node<T>* newNode = new Node<T>(d);
+    if(head) {
+        tail -> link = newNode;
+        tail = newNode;
+    }
+    else {
+        head = newNode;
+        tail = newNode;
+    }
+    size ++;
+}
+
+//dequeue function.
+template <class T>
+T Queue<T>::dequeue() {
+    T result = T();
+    if(empty()) {
+        return result;
+    }
+    Node<T>* temp = head;
+    result = temp -> data;
+    head = head -> link;
+    size --;
+    delete temp;
+    return result;
+}
+
+//clear function.
+template <class T>
+void Queue<T>::clear() {
+    while(head) {
+        Node<T>* temp = head;
+        head = head -> link;
+        delete temp;
+    }
+    head = nullptr;
+    tail = nullptr;
+    size = 0;
+}
+
+//Reverse function
+template <class T>
+void Queue<T>::reverse() {
+    if(size < 2) {
+        return;
+    }
+    Node<T>* previous = nullptr;
+    Node<T>* current = head;
+    Node<T>* next = nullptr;
+    
+    tail = current;
+    while(current) {
+        //Store the next node.
+        next = current -> link;
+        //Invert the current node's pointer
+        current -> link = previous;
+
+        //Move the pointers
+        previous = current;
+        current = next;
+    }
+    head = previous;
+}
+
+//Overloading the << operator.
+template <class T>
+ostream& operator <<(ostream& outs, const Queue<T>& q) {
+    int count(0);
+    outs << "[";
+    Node<T>* current = q.head;
+    while(current) {
+        outs << *current;
+        if(count < q.size - 1) {
+            outs << " -> ";
+            count ++;
+        }
+        current = current -> link;
+    }
+    outs << "]";
+    return outs;
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////Queue
+
+
+
+
 int main() {
     Stack<int> s;
     s.push(1);
     s.push(2);
     s.push(3);
     s.push(4);
+    s.push(5);
     cout << s << "\n";
     cout << "poping " << s.pop() << " from the stack\n";
     cout << s << "\n";
+    Stack<int> s2;
+    s.push(100);
+    s.push(34);
+    s2.push(12);
+    s2 = s;
+    cout << s2 << "\n";
+    cout << s << "\n";
+    cout << "Testing Queue\n";
+    Queue<int> q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    cout << q << "\n";
+    q.dequeue();
+    cout << q << "\n";
+    Queue<int> q2(q);
+    cout << q2 << "\n";
+    q.reverse();
+    cout << q << "\n";
     return 0;
 }
