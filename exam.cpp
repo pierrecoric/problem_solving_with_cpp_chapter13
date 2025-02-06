6e019ccd90f6a81153bbe5750709bf13eaf2e3d2
sheep <pierre.coric.lg@gmail.com>
2025-02-06 16:20:18 -0500

    exam

---
+++ exam.cpp
@@ -0,0 +1,21 @@
+truct TreeNode {
+int val;
+TreeNode* left;
+TreeNode* right;
+TreeNode(int val): val(val), left(nullptr), right(nullptr) {}
+};
+// Given two binary trees, write a function to check if they are the same or not.
+// Two binary trees are considered the same if they are structurally identical
+// and the nodes have the same value.
+bool isSameTree(TreeNode* p, TreeNode* q) {
+// TODO
+}
+// Given a binary tree, check whether it is a mirror of itself
+// (ie, symmetric around its center).
+bool isSymmetric(TreeNode* root) {
+// TODO
+}
+// Given a binary tree, implement level order traversal
+void levelOrder(TreeNode* root) {
+// TODO
+}
